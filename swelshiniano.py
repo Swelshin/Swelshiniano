1351db00a46eed7c4f1ce764e275885c404406ea
Guillermo leira temes <104579306+Guille-ux@users.noreply.github.com>
2025-02-07 22:42:34 +0100

    Update swelshiniano.py

--- swelshiniano.py
+++ swelshiniano.py
@@ -40,6 +40,25 @@ async def better_markov_gen(ctx, seed, numw, a):
     await ctx.send(response)
 @bot.command("helper")
 async def help_msg(ctx):
-	await ctx.send("COMMANDS WITH $ \n $random-text → makes a markov model to send a random message, use → $random-text [SEED] [number_of_words] [corpus, replace spaces with '_' and for separate things use ';'] \n $rtext → for do the same but in this time the system learns (no elimina el vocabulario y los datos de antes), use → the same \n $hola → Swelshiniano say's 'Pong' \n $helper → displays this message")
-
+    await ctx.send("COMMANDS WITH $ \n $random-text → makes a markov model to send a random message, use → $random-text [SEED] [number_of_words] [corpus, replace spaces with '_' and for separate things use ';'] \n $rtext → for do the same but in this time the system learns (no elimina el vocabulario y los datos de antes), use → the same \n $hola → Swelshiniano say's 'Pong' \n $helper → displays this message \n $reset-vocab → rests te vocabulary of the bot (at rtext) \n $save → saves the current vocab \n $load → loads the saved vocab\n$text → is the same than '$rtext' but in this you only write seed and number of words \n \n NOTA → save y load necesitan de un arguente que es el nombre del archivo")
+@bot.command("reset-vocab")
+async def reset(ctx):
+    global mm
+    mm.vocablo = []
+    await ctx.send("Vocablo Reseteado Correctamente")
+@bot.command("text")
+async def ret_text(ctx, a, b):
+    global mm
+    response = "".join(mm.ultigen(a, int(b)))
+    await ctx.send(response)
+@bot.command("save")
+async def save_vo(ctx, file):
+    global mm
+    mm.save_vocab(file)
+    await ctx.send("Vocablo Guardado con éxito")
+@bot.command("load")
+async def load_vo(ctx, file):
+    global mm
+    mm.load_vocab(file)
+    await ctx.send("Vocablo Cargado con éxito")
 bot.run(TOKEN)
